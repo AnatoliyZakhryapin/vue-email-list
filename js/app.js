f7133a296b1be025afacaeda9935d1a0d5cf7ae9
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-02 14:24:47 +0100

    Refactor function createMailList : - get name of mail from server boolean

--- js/app.js
+++ js/app.js
@@ -11,9 +11,15 @@ createApp({
     methods: {
         createMailList() {
             for(let i = 0; i < this.numberOfList; i++){
-                const n = i + 1;
-                this.mailList.push("Mail" + " " + n);
-                console.log(this.mailList);
+                // const n = i + 1;
+                // this.mailList.push("Mail" + " " + n);
+                // console.log(this.mailList);
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then((response) => {
+                        console.log(response.data)
+                        this.mailList.push(response.data.response);
+                    })
             }
         }
     },
