be8635cd0e008aa15d8399f53a1c494377b9f00c
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-11-02 14:16:38 +0100

    ADD function createMailList

--- js/app.js
+++ js/app.js
@@ -3,7 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {  
-            indirizzo: "Mail"         
+            // indirizzo: "Mail",
+            numberOfList: 10,
+            mailList: []        
         }
-    }
+    },
+    methods: {
+        createMailList() {
+            for(let i = 0; i < this.numberOfList; i++){
+                const n = i + 1;
+                this.mailList.push("Mail" + " " + n);
+                console.log(this.mailList);
+            }
+        }
+    },
+    created() {
+		this.createMailList()
+	},
 }).mount("#app")
